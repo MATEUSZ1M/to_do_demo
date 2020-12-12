4cc67fb7c4078352bbd9bc573dc355e1bec8f1e6
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-12-12 23:44:26 +0100

    Add logic of saving items in localstorage

--- js/script.js
+++ js/script.js
@@ -20,6 +20,8 @@ const addToDo = (e) => {
   newToDo.classList.add("todo-item");
   //apend child to div
   todoDiv.appendChild(newToDo);
+  //add to do to local storage
+  saveLocalTodos(toDoInput.value);
   //check mark button
   const completedButton = document.createElement("button");
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -37,9 +39,7 @@ const addToDo = (e) => {
   //clear input value
   toDoInput.value = "";
 };
-
 //function deliting or check activation
-
 const deleteCheck = (e) => {
   const item = e.target;
 
@@ -59,32 +59,44 @@ const deleteCheck = (e) => {
     todo.classList.toggle("completed");
   }
 };
-
 //function filter todos by status
-function filterToDo(e) {
+const filterToDo = (e)=> {
   const todos = toDoList.childNodes;
-  todos.forEach(function(todo){
-    switch(e.target.value) {
-      case 'all':
-        todo.style.display = 'flex';
+  todos.forEach((todo)=> {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
         break;
-      case 'completed':
-        if(todo.classList.contains('completed')){
-          todo.style.display = 'flex';
-        }else{
-          todo.style.display = 'none';
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
         }
         break;
-        case 'uncompleted':
-        if(!todo.classList.contains('completed')){
-          todo.style.display = 'flex';
-        }else{
-          todo.style.display = 'none';
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
         }
+        break;
     }
-  })
+  });
 }
+// save to dos to local storage
+const saveLocalTodos = (todo) => {
+  let todos; 
 
+  if(localStorage.getItem('todos') === null){
+    todos = []
+  }else{
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+
+  todos.push(todo);
+  localStorage.setItem('todos', JSON.stringify(todos))
+}
 //event listener
 toDoButton.addEventListener("click", addToDo);
 toDoList.addEventListener("click", deleteCheck);
