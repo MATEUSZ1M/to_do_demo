311a8c28ceafeda8b4917471854c2b09481bff41
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-12-12 19:04:10 +0100

    Add Logic to adding text to the list items

--- js/script.js
+++ js/script.js
@@ -1,37 +1,49 @@
 //selectors
-const toDoInput = document.querySelector('.todo-input');
-const toDoButton = document.querySelector('.todo-button');
-const toDoList= document.querySelector('.todo-list ');
+const toDoInput = document.querySelector(".todo-input");
+const toDoButton = document.querySelector(".todo-button");
+const toDoList = document.querySelector(".todo-list ");
 
-//function
-const addToDo =(e)=> {
+//function adding to do
+const addToDo = (e) => {
   //prevent form from subliting
-  e.preventDefault()
+  e.preventDefault();
   //create div
-  const todoDiv = document.createElement('div');
+  const todoDiv = document.createElement("div");
   //add class to div
-  todoDiv.classList.add('todo');
+  todoDiv.classList.add("todo");
   //create li inside div
-  const newToDo = document.createElement('li');
+  const newToDo = document.createElement("li");
   //add text
-  newToDo.innerText = 'dupa'//temporary
+  newToDo.innerText = toDoInput.value;
   //add class.
-  newToDo.classList.add('todo-item')
+  newToDo.classList.add("todo-item");
   //apend child to div
   todoDiv.appendChild(newToDo);
   //check mark button
-  const completedButton = document.createElement('button');
-  completedButton.innerHTML = '<i class="fas fa-check"></i>'
-  completedButton.classList.add('complete-btn');
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
   todoDiv.appendChild(completedButton);
   //trash button
-  const trashButton = document.createElement('button');
-  trashButton.innerHTML = '<i class="fas fa-trash"></i>'
-  trashButton.classList.add('trash-btn');
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
-  //ad created div to the list
-  toDoList.appendChild(todoDiv);
-  console.log(todoDiv)
+  //prevent from adding empty li
+  if(toDoInput.value !== ''){
+    toDoList.appendChild(todoDiv);
+  }else alert('write sometching first 🦥')
+  //clear input value
+  toDoInput.value = ''
+};
+
+//function deliting or check activation
+
+const deleteCheck =()=>{
+  
 }
+
+
 //event listener
-toDoButton.addEventListener('click', addToDo);
+toDoButton.addEventListener("click", addToDo);
+toDoList.addEventListener('click', deleteCheck);
